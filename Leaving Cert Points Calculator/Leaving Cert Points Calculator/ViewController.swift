066ad3f939c1f9cfb662c15e77e634035b93f5e6
aherrmann93 <adam.herrmann@ucdconnect.ie>
2017-07-30 00:02:14 +0100

    First iteration at basic UI

---
+++ Leaving Cert Points Calculator/Leaving Cert Points Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leaving Cert Points Calculator
+//
+//  Created by Adam Herrmann on 27/07/2017.
+//  Copyright © 2017 Adam Herrmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
